c0564b44a71ac02ba695bd27d62db3d5022edc21
mayukhDevOps <128017823+mayukhDevOps@users.noreply.github.com>
2024-09-06 13:33:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// aleart("hello") // we are using node.js, not browser
+
+let Id = "Mayukh"
+let age = 21
+let address = "Rampur"
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+object 
+*/
+
+console.log(typeof undefined) // used to state the datatypes
+
+//null is an object datatype.//
