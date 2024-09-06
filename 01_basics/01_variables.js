ba12c606e62e8895d9c1be13c4b312917dec017e
mayukhDevOps <128017823+mayukhDevOps@users.noreply.github.com>
2024-09-06 13:29:59 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12
+let accountEmail = "mpk@gmail.com"
+var accountPassword = "1212"
+accountCity = "Mirza"
+let accountState;
+
+//accountId = 21 // not allowed
+accountEmail = "mpdon@gmail.com"
+accountPassword = "4545"
+accountCity = "Guwahati"
+
+console.log(accountState)
