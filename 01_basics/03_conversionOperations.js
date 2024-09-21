4adaa686fd0dd4146c34c9af7819ccd810f1add5
mayukhDevOps <128017823+mayukhDevOps@users.noreply.github.com>
2024-09-21 05:24:45 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = false
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "35" => 35
+// "35abc" => NaN 
+// ture => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "mayukh" => true
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
